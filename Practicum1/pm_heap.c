8dbf294288176edf0ffc7fd32db7a54563d1c251
Cabeza <caoning3708@gmail.com>
2023-03-19 18:02:40 -0400

    change pysical address if swapping out a page, add timestamp to pm_read if the page in the memory (#1)

--- Practicum1/pm_heap.c
+++ Practicum1/pm_heap.c
@@ -5,10 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static char pm_heap[PM_HEAP_SIZE];
-static pm_page_table_entry pm_page_table[2 * PM_NUM_PAGES];
+static char pm_heap[PM_HEAP_SIZE];                          // memory heap
+static pm_page_table_entry pm_page_table[2 * PM_NUM_PAGES]; // size = memory + disk
 pthread_mutex_t pm_mutex;
-static size_t timestamp_counter = 0;
+static size_t timestamp_counter = 0; // LRU timestamp
 FILE *disk_file;
 
 void pm_init(void)
@@ -21,7 +21,7 @@ void pm_init(void)
         pm_page_table[i].timestamp = 0;
         pm_page_table[i].physical_address = (i % PM_NUM_PAGES) * PM_PAGE_SIZE;
         pm_page_table[i].disk_offset = i * PM_PAGE_SIZE;
-        pm_page_table[i].dirty = 0;
+        pm_page_table[i].present = 0;
     }
     disk_file = fopen(DISK_FILE, "wb+");
     if (!disk_file)
@@ -37,7 +37,7 @@ size_t find_lru_page(void)
     size_t lru_page = 0;
     for (size_t i = 0; i < PM_NUM_PAGES; i++)
     {
-        if (!pm_page_table[i].free && pm_page_table[i].timestamp < min_timestamp && !pm_page_table[i].dirty)
+        if (!pm_page_table[i].free && pm_page_table[i].timestamp < min_timestamp && !pm_page_table[i].present)
         {
             min_timestamp = pm_page_table[i].timestamp;
             lru_page = i;
@@ -46,25 +46,26 @@ size_t find_lru_page(void)
     return lru_page;
 }
 
-void swap_out(size_t lru_page)
+void *swap_out(size_t lru_page)
 {
     fseek(disk_file, pm_page_table[lru_page].disk_offset, SEEK_SET);
     fwrite(pm_heap + pm_page_table[lru_page].physical_address, 1, PM_PAGE_SIZE, disk_file);
     printf("\nSwapping out page %d, contents: %s\n", (int)lru_page, pm_heap + pm_page_table[lru_page].physical_address);
     pm_page_table[lru_page].free = 1;
     pm_page_table[lru_page].size = 0;
-    pm_page_table[lru_page].dirty = 0;
+    pm_page_table[lru_page].present = 1; // ????
+    return (void *)(pm_page_table[lru_page].physical_address);
 }
 
-void swap_in(size_t disk_page, size_t new_page)
+void swap_in(size_t disk_page, size_t lru_page)
 {
     fseek(disk_file, pm_page_table[disk_page].disk_offset, SEEK_SET);
-    fread(pm_heap + pm_page_table[new_page].physical_address, 1, PM_PAGE_SIZE, disk_file);
-    printf("\nSwapping in page %d, contents: %s\n", (int)disk_page, pm_heap + pm_page_table[new_page].physical_address);
-    pm_page_table[new_page].free = 0;
-    pm_page_table[new_page].size = PM_PAGE_SIZE;
-    pm_page_table[new_page].timestamp = ++timestamp_counter;
-    pm_page_table[new_page].dirty = 0;
+    fread(pm_heap + pm_page_table[lru_page].physical_address, 1, PM_PAGE_SIZE, disk_file);
+    printf("\nSwapping in page %d, contents: %s\n", (int)disk_page, pm_heap + pm_page_table[lru_page].physical_address);
+    pm_page_table[disk_page].free = 0;
+    pm_page_table[disk_page].size = PM_PAGE_SIZE;
+    pm_page_table[disk_page].timestamp = ++timestamp_counter;
+    pm_page_table[disk_page].present = 0;
 }
 
 size_t pm_malloc(size_t size)
@@ -73,7 +74,8 @@ size_t pm_malloc(size_t size)
 
     size_t free_page = 2 * PM_NUM_PAGES - 1;
     size_t free_page_count = 0;
-
+    size_t empty_address = 0;
+    // find non free page num
     for (size_t i = 0; i < 2 * PM_NUM_PAGES; i++)
     {
         if (!pm_page_table[i].free)
@@ -85,23 +87,33 @@ size_t pm_malloc(size_t size)
     if (free_page_count >= PM_NUM_PAGES)
     {
         size_t lru_page = find_lru_page();
-        // printf("\nLRU page: %d\n", (int)lru_page);
-        swap_out(lru_page);
+        printf("\nLRU page: %d\n", (int)lru_page);
+        empty_address = (size_t)swap_out(lru_page);
     }
-    // Find a new free page table entry for the new page, that is not dirty
+
+    // Find a new free page table entry for the new page, that is not present
     for (size_t i = 0; i < 2 * PM_NUM_PAGES; i++)
     {
-        if (pm_page_table[i].free && !pm_page_table[i].dirty)
+        if (pm_page_table[i].free && !pm_page_table[i].present)
         {
             free_page = i;
             printf("\nFree page: %d\n", (int)free_page);
             break;
         }
     }
+
     pm_page_table[free_page].free = 0;
     pm_page_table[free_page].size = size;
     pm_page_table[free_page].timestamp = ++timestamp_counter;
     pm_page_table[free_page].disk_offset = free_page * PM_PAGE_SIZE;
+    if (empty_address != 0)
+    {
+        pm_page_table[free_page].physical_address = empty_address;
+    }
+    else
+    {
+        pm_page_table[free_page].physical_address = (free_page % PM_NUM_PAGES) * PM_PAGE_SIZE;
+    }
 
     pthread_mutex_unlock(&pm_mutex);
 
@@ -115,7 +127,7 @@ void pm_free(size_t virtual_address)
     {
         pm_page_table[virtual_address].free = 1;
         pm_page_table[virtual_address].size = 0;
-        pm_page_table[virtual_address].dirty = 0;
+        pm_page_table[virtual_address].present = 0;
     }
 
     pthread_mutex_unlock(&pm_mutex);
@@ -132,7 +144,7 @@ void *pm_write(size_t virtual_address, char *data, size_t size)
     if (size <= pm_page_table[virtual_address].size)
     {
         memcpy(pm_heap + pm_page_table[virtual_address].physical_address, data, size);
-        // pm_page_table[virtual_address].dirty = 1;
+        // pm_page_table[virtual_address].present = 1;
     }
     else
     {
@@ -156,21 +168,15 @@ void *pm_read(size_t virtual_address)
 
     if (!pm_page_table[virtual_address].free)
     {
-        if (pm_page_table[virtual_address].dirty)
+        if (pm_page_table[virtual_address].present)
         {
-            size_t free_page = 2 * PM_NUM_PAGES;
             size_t free_page_count = 0;
 
             for (size_t i = 0; i < 2 * PM_NUM_PAGES; i++)
             {
-                if (pm_page_table[i].free && !pm_page_table[i].dirty)
+                if (pm_page_table[i].free && !pm_page_table[i].present)
                 {
                     free_page_count++;
-                    if (free_page_count >= PM_NUM_PAGES)
-                    {
-                        free_page = i;
-                        break;
-                    }
                 }
             }
 
@@ -182,11 +188,18 @@ void *pm_read(size_t virtual_address)
             }
             else
             {
-                swap_in(virtual_address, free_page);
+                swap_in(virtual_address, virtual_address);
             }
         }
-
+        else
+        {
+            // in memory, not present, update timestamp
+            pm_page_table[virtual_address].timestamp = ++timestamp_counter;
+        }
+        // printf page number
+        printf("\nReading page %d, contents: %s\n", (int)virtual_address, pm_heap + pm_page_table[virtual_address].physical_address);
         pthread_mutex_unlock(&pm_mutex);
+
         return (void *)(pm_heap + pm_page_table[virtual_address].physical_address);
     }
 
